122cd90495260fe92488932d1825dfae720c5a53
Giorgio Giannotta <jorge_upa@msn.com>
2023-01-01 10:02:49 +0100

    Fix HighScore and Court Image

--- BasketballHoops/GameScene.swift
+++ BasketballHoops/GameScene.swift
@@ -17,8 +17,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var scoreLabel : SKLabelNode?
     private var timerLabel : SKLabelNode?
     
+    private var scoreLabelNumber : SKLabelNode?
+    private var bestScoreLabel : SKLabelNode?
+    
     var yourScoreLabel : SKLabelNode?
-    var numberScoreLabel : SKLabelNode?
+    var numberBestScoreLabel : SKLabelNode?
     
     let mainMenuButton = SKSpriteNode(imageNamed: "mainMenuButton")
     
@@ -38,6 +41,56 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func sceneDidLoad() {
         initGame()
+        scoreLabel = SKLabelNode(fontNamed: "Arial")
+        scoreLabel?.text = "Score"
+        scoreLabel?.position = CGPoint(x: -78, y: 420)
+        scoreLabel?.fontSize = 30
+        scoreLabel?.fontColor = #colorLiteral(red: 1, green: 0.2963023484, blue: 0, alpha: 1)
+        scoreLabel?.zPosition = -1
+        if scoreLabel != nil {
+            addChild(scoreLabel!)
+        }
+        
+        scoreLabelNumber = SKLabelNode(fontNamed: "Arial")
+        scoreLabelNumber?.text = "0"
+        scoreLabelNumber?.position = CGPoint(x: -78, y: 340)
+        scoreLabelNumber?.fontSize = 50
+        scoreLabelNumber?.fontColor = #colorLiteral(red: 1, green: 0.2963023484, blue: 0, alpha: 1)
+        scoreLabelNumber?.zPosition = -1
+        if scoreLabelNumber != nil {
+            addChild(scoreLabelNumber!)
+        }
+        
+        bestScoreLabel = SKLabelNode(fontNamed: "Arial")
+        bestScoreLabel?.text = "Best Score"
+        bestScoreLabel?.position = CGPoint(x: 88, y: 420)
+        bestScoreLabel?.fontSize = 30
+        bestScoreLabel?.fontColor = #colorLiteral(red: 0.4818688035, green: 1, blue: 0.2141556144, alpha: 1)
+        bestScoreLabel?.zPosition = -1
+        if bestScoreLabel != nil {
+            addChild(bestScoreLabel!)
+        }
+        
+        numberBestScoreLabel = SKLabelNode(fontNamed: "Arial")
+        numberBestScoreLabel?.text = "\(UserDefaults.standard.integer(forKey: kBestScore))"
+        numberBestScoreLabel?.position = CGPoint(x: 88, y: 340)
+        numberBestScoreLabel?.fontSize = 50
+        numberBestScoreLabel?.fontColor = #colorLiteral(red: 0.4818688035, green: 1, blue: 0.2141556144, alpha: 1)
+        numberBestScoreLabel?.zPosition = -1
+        if numberBestScoreLabel != nil {
+            addChild(numberBestScoreLabel!)
+        }
+        
+        timerLabel = SKLabelNode(fontNamed: "Arial")
+        timerLabel?.position = CGPoint(x: 0, y: 480)
+        timerLabel?.text = "Time Left: \(seconds)"
+        timerLabel?.fontSize = 50
+        timerLabel?.fontColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        timerLabel?.zPosition = -1
+        if timerLabel != nil {
+            addChild(timerLabel!)
+        }
+        
     }
     
     override func didMove(to view: SKView) {
@@ -55,8 +108,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         startingPosition?.physicsBody?.categoryBitMask = startingPositionCategory
         startingPosition?.physicsBody?.contactTestBitMask = ballCategory
         
-        scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
-        timerLabel = childNode(withName: "timerLabel") as? SKLabelNode
        
     }
     
@@ -75,8 +126,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             if node.name == "mainMenu" {
                 node.removeFromParent()
                 scene?.isPaused = false
-                yourScoreLabel?.removeFromParent()
-                numberScoreLabel?.removeFromParent()
                 initGame()
            }
         }
@@ -89,13 +138,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if contact.bodyA.categoryBitMask == hoopCategory {
             print("Point Shot A")
             score += 2
-            scoreLabel?.text = "Score: \(score)"
+            scoreLabelNumber?.text = "\(score)"
 
         }
         if contact.bodyB.categoryBitMask == hoopCategory {
             print("Point Shot B")
             score += 2
-            scoreLabel?.text = "Score: \(score)"
+            scoreLabelNumber?.text = "\(score)"
 
         }
         
@@ -113,46 +162,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+//        numberBestScoreLabel = SKLabelNode(fontNamed: "Arial")
+//        numberBestScoreLabel?.text = "\(UserDefaults.standard.integer(forKey: kBestScore))"
+//        numberBestScoreLabel?.position = CGPoint(x: 88, y: 340)
+//        numberBestScoreLabel?.fontSize = 50
+//        numberBestScoreLabel?.fontColor = #colorLiteral(red: 0.4818688035, green: 1, blue: 0.2141556144, alpha: 1)
+//        numberBestScoreLabel?.zPosition = -1
+//        if numberBestScoreLabel != nil {
+//            addChild(numberBestScoreLabel!)
+//        }
     }
     
     func gameOver() {
-        //
             scene?.isPaused = true
             gameTimer?.invalidate()
-            scoreLabel?.alpha = 0.5
-            score = 0
             seconds = 60
-            
-            
-            //            let controller = self.view?.window?.rootViewController as! GameViewController
-            //            controller.showInterstitialAd()
-            //
-            
-            yourScoreLabel = SKLabelNode()
-            yourScoreLabel?.text = "Best Score"
-            yourScoreLabel?.position = CGPoint(x: 0, y: 200)
-            yourScoreLabel?.fontSize = 50
-            yourScoreLabel?.fontColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            yourScoreLabel?.zPosition = 30
-            if yourScoreLabel != nil {
-                addChild(yourScoreLabel!)
-            }
             
             UserDefaults.standard.set(score, forKey: kScore)
             if score > UserDefaults.standard.integer(forKey: kBestScore) {
                 UserDefaults.standard.set(score, forKey: kBestScore)
             }
-            numberScoreLabel = SKLabelNode()
-            numberScoreLabel?.text = "\(UserDefaults.standard.integer(forKey: kBestScore))"
-            numberScoreLabel?.position = CGPoint(x: 0, y: 50)
-            numberScoreLabel?.fontSize = 100
-            numberScoreLabel?.fontColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            numberScoreLabel?.zPosition = 30
-            if numberScoreLabel != nil {
-                addChild(numberScoreLabel!)
-            }
-            
-            
+            numberBestScoreLabel?.text = "\(UserDefaults.standard.integer(forKey: kBestScore))"
+
+            score = 0
+        
             mainMenuButton.position = CGPoint(x: 0, y: -100)
             mainMenuButton.name = "mainMenu"
             mainMenuButton.zPosition = 30
@@ -163,7 +196,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if seconds >= 0 {
             gameTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
                 self.seconds -= 1
-                self.timerLabel?.text = "Time: \(self.seconds)"
+                self.timerLabel?.text = "Time Left: \(self.seconds)"
                 print("\(self.seconds)")
                 if self.seconds <= 0 {
                     self.gameOver()
